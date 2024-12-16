a58638a629e04d97dacb5b33f7557c0c784bae3f
Jérémy Tarlié <contact@tarlie.fr>
2024-12-16 16:53:12 +0100

    modif command

--- app/[locale]/[404]/page.tsx
+++ app/[locale]/[404]/page.tsx
@@ -1,12 +1,13 @@
 import { useTranslations } from "next-intl";
 import Image from "next/image";
+import style from "@/public/style/404.module.css";
 
 export default function LocalePage() {
   const t = useTranslations("404");
 
   return (
-    <main>
-      <Image src="/img/profile-bot.png" width={200} height={200} alt="404" />
+    <main className={style.container_error}>
+      <Image src="/img/error-404.jpg" width={200} height={200} alt="404" />
       <p>{t("title")}</p>
       <p>{t("description")}</p>
       <a href="/">{t("back")}</a>
